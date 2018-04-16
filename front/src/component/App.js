01c0dac99ec4821660558ef04055cf294bec1942
Mingfei Huang <himax1023@gmail.com>
2018-04-16 10:09:36 -0400

    add navbar

--- front/src/component/App.js
+++ front/src/component/App.js
@@ -13,7 +13,8 @@ import * as api from '../common/api';
 import Home from './Home';
 import OAuth_callback from './OAuth_callback';
 import Welcome from './Welcome';
-
+import Navbar from './Navbar';
+ 
 import {
   Router,
   Switch,
@@ -72,6 +73,7 @@ class App extends Component {
     return (
       <div className="app">
         <ToastContainer position='top-center' hideProgressBar={true} />
+        <Navbar />
         <Router history={this.history}>
           <Switch>
             <Route exact path="/home" component={Home} />
---
+++ front/src/component/Navbar.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Image, Menu, Popup } from 'semantic-ui-react';
+
+class Navbar extends Component {
+
+  constructor() {
+    super();
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleLogout() {
+    this.props.dispatch({
+      type: 'SET_GH_USER',
+      access_token: undefined,
+      user: undefined,
+    });
+    window.location.href = '/';
+  }
+
+  renderUserAvatar() {
+    return (
+      <div>
+        <Menu.Item >
+          <Image size="mini" circular src={this.props.user.avatar_url} />
+        </Menu.Item>
+      </div>
+    );
+  }
+
+  renderLoggedIn() {
+    return (
+      <Popup wide trigger={this.renderUserAvatar()} on='click' position='bottom center' >
+        <Menu fluid vertical>
+          <Menu.Item onClick={this.handleLogout}>Logout</Menu.Item>
+        </Menu>
+      </Popup>
+    );
+  }
+
+  renderNotLogIn() {
+    // not used for now
+    return (
+      null
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Menu pointing secondary>
+          <Menu.Item name='Twin' as='a' href='/' />
+          <Menu.Menu position='right'>
+            {
+              this.props.user ? this.renderLoggedIn() : this.renderNotLogIn()
+            }
+          </Menu.Menu>
+        </Menu>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  user: state.user
+});
+
+export default connect(mapStateToProps)(Navbar);
